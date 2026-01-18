23f98af09952bddd3b13b62596c9b161e0bba8c5
KuLiPai <yun.svip@gmail.com>
2026-01-18 21:07:07 +0800

    refactor(gradle): 升级kotlin2.3.0，agp到8.11.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,13 +10,13 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-    // 在这里集中管理所有插件的版本
-    plugins {
-        id("com.android.application") version "8.11.0"
-        id("com.android.library") version "8.11.0"
-        id("org.jetbrains.kotlin.android") version "2.2.0"
-        id("org.jetbrains.kotlin.jvm") version "2.2.0"
-    }
+//    // 在这里集中管理所有插件的版本
+//    plugins {
+//        id("com.android.application") version "8.11.2"
+//        id("com.android.library") version "8.11.2"
+//        id("org.jetbrains.kotlin.android") version "2.3.0"
+//        id("org.jetbrains.kotlin.jvm") version "2.3.0"
+//    }
 }
 
 
