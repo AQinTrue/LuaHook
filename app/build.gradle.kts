4aace09d4f44b3bdeb679d75dd26474595d29e2b
KuLiPai <yun.svip@gmail.com>
2026-01-30 00:49:54 +0800

    fix(native): 修复release编译失败问题

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,9 @@ android {
                 cppFlags += ""
             }
         }
+        ndk {
+            abiFilters += listOf("armeabi-v7a", "arm64-v8a")
+        }
     }
 
     buildTypes {
